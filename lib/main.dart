4ad7ed54e5593f6ef0e345364564d73259ff74a6
Kamlesh Kumar <kamlesh.kumar@agreeya.com>
2020-08-24 14:23:06 +0530

    grid and list view added added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -46,8 +48,72 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+//  _MyHomePageState createState() => _MyHomePageState();
+  _MyListPageState createState() => _MyListPageState();
 }
+
+
+class _MyListPageState extends State<MyHomePage> {
+  static final showGrid = false; // Set to false to show ListView
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My List App"),
+      ),
+      body: Center(child: _buildList()), //body: Center(child: showGrid ? _buildGrid() : _buildList()),
+    );
+  }
+
+
+  Widget _buildGrid() => GridView.extent(
+    maxCrossAxisExtent: 150,
+    mainAxisSpacing: 4,
+    crossAxisSpacing: 4,
+    padding: const EdgeInsets.all(4),
+    children: _buildGridTitleList(30),
+  );
+
+  // #docregion list
+  Widget _buildList() => ListView(
+    children: [
+      _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+      _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+      _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+      _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+      _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+          Icons.theaters),
+      _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+      Divider(),
+      _tile('Kescaped_code#39;s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+      _tile('Emmyescaped_code#39;s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+      _tile(
+          'Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+      _tile('La Ciccia', '291 30th St', Icons.restaurant),
+    ],
+  );
+
+  ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
+    title: Text(title,
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        )),
+    subtitle: Text(subtitle),
+    leading: Icon(
+      icon,
+      color: Colors.blue[500],
+    ),
+  );
+
+  List<Container> _buildGridTitleList(int count) => List.generate(count, (index) =>
+      Container(child: Image.asset("images/pic$index.jpg"),)
+  );
+
+}
+
 
 class _MyHomePageState extends State<MyHomePage> {
 
@@ -58,23 +124,93 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("My First App"), //widget.title
       ),
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              height: 100,
-              color: Colors.red,
+      body: Container(
+        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+        margin: EdgeInsets.all(2.5),
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:<Widget> [
+            Text("Text1"),
+            Text("Text2"),
+            Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget> [
+                  ListTile(
+                      title: Text("ABC", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),)
+                  ),
+                  Divider(color: Colors.black12,),
+                  ListTile(
+                    title: Text("EFG", style: CustomFontStyle.textPrimary(),),
+                  ),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: ContainerWidget(colorName: Colors.blue),
-          ),
-          Expanded(
-            child: ContainerWidget(colorName: Colors.green),
-          ),
-        ],
+//            Stack(
+//              alignment: const Alignment(0.6, 0.8),
+//              children: <Widget>[
+//                Container(
+//                  height: 200,
+//                  width: 200,
+//                  color: Colors.blue,
+//                ),
+//                Container(
+//                  height: 150,
+//                  width: 150,
+//                  color: Colors.white24,
+//                ),
+//                Container(
+//                  decoration: BoxDecoration(
+//                    color: Colors.black45,
+//                  ),
+//                  child: Text(
+//                    "Hello Sonali",
+//                    style: TextStyle(
+//                        fontSize: 20,
+//                        color: Colors.white,
+//                        fontWeight: FontWeight.bold
+//                    ),
+//                  ),
+//                )
+//              ],
+//            ),
+            SizedBox(
+              height: 210,
+              child: Card(
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text("1625 Main Street",
+                        style: TextStyle(fontWeight: FontWeight.w500),),
+                      subtitle: Text("My City, CA 99820"),
+                      leading: Icon(
+                        Icons.restaurant_menu,
+                        color: Colors.blue[500]
+                      ),
+                    ),
+                    Divider(color: Colors.black12,),
+                    ListTile(
+                      title: Text("(423)382-9232",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500
+                      ),),
+                      leading: Icon(
+                        Icons.contact_phone,
+                        color: Colors.blue[500],
+                      ),
+                    ),
+                    ListTile(
+                      title: Text("contact@myresturant.com", style: TextStyle(fontWeight: FontWeight.w500),),
+                      leading: Icon(Icons.email, color: Colors.blue[500],),
+                    )
+                  ],
+                ),
+              ),
+            )
+
+          ],
+        ),
       ),
       drawer: Drawer(
         child: ListView(
@@ -108,5 +244,11 @@ class ContainerWidget extends StatelessWidget {
       height: 100,
       color: this.colorName,
     );
+  }
+}
+
+class CustomFontStyle {
+  static TextStyle textPrimary() {
+    return TextStyle(fontSize: 16, color: Colors.blue, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic);
   }
 }
